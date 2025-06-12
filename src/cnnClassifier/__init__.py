e94009be6ae548364239cff79cebb0cad57da442
snigdhrf <snigdhsingh94@gmail.com>
2025-06-12 17:08:18 +0530

    Utils, Exception & Logger added

---
+++ src/cnnClassifier/__init__.py
@@ -0,0 +1,26 @@
+"""This code sets up a custom logging configuration for the project. 
+Logging is used to track events while code runs — such as progress, warnings, or errors — and this setup ensures 
+those events are recorded both to a file and to the console."""
+
+import os
+import sys
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("cnnClassifierLogger")
